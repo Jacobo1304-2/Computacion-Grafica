055a5330fed340361c2fb9641d14e181f72f0328
andruAJ <giraldoandresjuan0405@gmail.com>
2025-10-23 10:04:11 -0500

    Puse las cosas que me faltaban del efecto de blur de la clase pasada

--- Assets/Scenes/AndresJuan/Shaders_AJ/PostProcesing/Include/GaussianBlur.hlsl
+++ Assets/Scenes/AndresJuan/Shaders_AJ/PostProcesing/Include/GaussianBlur.hlsl
@@ -1,6 +1,10 @@
 #ifndef GAUSSIAN_BLUR_INCLUDED
 #define GAUSSIAN_BLUR_INCLUDED
 
+#ifndef _BlitTexture
+    TEXTURE2D(_BlitTexture);
+#endif
+
 void ThreeTapBoxBlur_float(float2 uv, float2 screenSize, float offset, out float3 Filtered)
 {
     Filtered = 0;
@@ -10,7 +14,7 @@ void ThreeTapBoxBlur_float(float2 uv, float2 screenSize, float offset, out float
         {
             
             float2 offUV = uv * screenSize + float2(x, y) * screenSize * offset;
-            float3 pixelValue = LOAD_TEXTURE2D_LOD(_BlitTexture. clamp(offUV, 0, 1), 0);
+            float3 pixelValue = LOAD_TEXTURE2D_LOD(_BlitTexture, clamp(offUV, 0, 1), 0);
             Filtered += pixelValue / 9.0f;
         }
     }
